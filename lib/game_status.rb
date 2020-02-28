ea384285927d4016d9894e1c5b1d89a26e9c5f33
Sarah Rose <sarahrose63089@gmail.com>
2020-02-28 00:53:55 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,22 +15,18 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  puts "won?"
   empty_board = board.all? do |space|
     space == " "
   end
   if empty_board
-    puts "returning false"
     return false
   end
   WIN_COMBINATIONS.each do |array|
     all_taken = position_taken?(board, array[0]) && position_taken?(board, array[1]) && position_taken?(board, array[2])
     if all_taken && board[array[0]] == board[array[1]] && board[array[0]] == board[array[2]]
-      print "return array", array, "\n"
       return array
     end
   end
-  puts "1-returning false"
   false
 end
 
@@ -41,32 +37,22 @@ def full?(board)
 end
 
 def draw?(board)
-  #returns true if the board has not been won but is full
-  #false if the board is not won and the board is not full
-  puts "draw?"
   if won?(board)
-    puts "returning false"
     return false
   end
   if full?(board)
-    puts "returning true"
     return true
   end
   false
 end
 
 def over?(board)
-  print "over?", board, "\n"
   if won?(board)
-    puts "won true"
     return true
   end
-  print draw?(board), "test"
   if draw?(board)
-    puts "draw true"
     return true
   end
-  puts "over-returning false"
   false #otherwise false--what's interfering here?
 end
 
